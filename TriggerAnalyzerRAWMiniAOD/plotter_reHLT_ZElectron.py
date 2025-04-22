5246904658c63c799bd7698cdba881bb98f58125
elfontan <elisa.fontanesi@cern.ch>
2025-04-22 23:20:10 +0200

    Fixed effective areas application

--- TriggerAnalyzerRAWMiniAOD/plotter_reHLT_ZElectron.py
+++ TriggerAnalyzerRAWMiniAOD/plotter_reHLT_ZElectron.py
@@ -20,13 +20,13 @@ output_dir = "/eos/user/e/elfontan/www/RUN3_LowMassDiPhoton/ZElectron/HISTOS_PtE
 variables = {
     "hoe":      {"title": "H over E",   "nbins": 200, "xmin": -2.,   "xmax": 2., "xlabel": r"H/E", "log": False},
     "hcaliso":  {"title": "HCAL Iso",   "nbins": 200, "xmin": -10.,   "xmax": 10., "xlabel": r"HCal isolation", "log": False},
-    "ecaliso":  {"title": "ECAL Iso",   "nbins": 100, "xmin": -2.,   "xmax": 6., "xlabel": r"ECal isolation", "log": False},
-    "trackiso": {"title": "Track Iso",  "nbins": 100, "xmin": -4., "xmax": 6., "xlabel": r"Track isolation", "log": False},
     "customecaliso":  {"title": "Custom ECAL Iso",   "nbins": 100, "xmin": -2.,   "xmax": 6., "xlabel": r"Custom ECal isolation", "log": False},
     "customecaliso2":  {"title": "Custom ECAL Iso (2)",   "nbins": 100, "xmin": -2.,   "xmax": 6., "xlabel": r"Custom ECal isolation (2)", "log": False},
     "custom1trackiso": {"title": "Custom Track Iso",  "nbins": 100, "xmin": -4., "xmax": 6., "xlabel": r"Custom Track isolation", "log": False},
     "custom2trackiso": {"title": "Custom Track Iso (2)",  "nbins": 100, "xmin": -4., "xmax": 6., "xlabel": r"Custom Track isolation (2)", "log": False},
-    #"hoeXe":      {"title": "H part of Hover E",   "nbins": 200, "xmin": -2.,   "xmax": 100., "xlabel": r"H/E x E", "log": False},
+    ####"ecaliso":  {"title": "ECAL Iso",   "nbins": 100, "xmin": -2.,   "xmax": 6., "xlabel": r"ECal isolation", "log": False},
+    ####"trackiso": {"title": "Track Iso",  "nbins": 100, "xmin": -4., "xmax": 6., "xlabel": r"Track isolation", "log": False},
+    ####"hoeXe":      {"title": "H part of Hover E",   "nbins": 200, "xmin": -2.,   "xmax": 100., "xlabel": r"H/E x E", "log": False},
 }
 
 # Define eta bins and effective areas
@@ -34,21 +34,22 @@ variables = {
 ea_eta_bins = [0.0, 1.0, 1.479, 2.1]
 ea_values = {
     "hoe": [0.066, 0.14, 0.3, 0.5],
-    "ecal": [0.1, 0.08, 0.06, 0.06],
     "hcal": [0.26, 0.32, 0.4, 0.5],
-    "track": [0.03, 0.04, 0.114, 0.032],
-    "eCalPf1": [0.1, 0.08, 0.06, 0.06],
-    "eCalPf2": [0.24, 0.2, 0.16, 0.14],
-    "trIso1": [0.14, 0.14, 0.16, 0.16],
-    "trIso2": [0.2, 0.2, 0.2, 0.2],
-    #"hoeXe": [0.066, 0.14, 0.3, 0.5]
+    "ecaliso1": [0.1, 0.08, 0.06, 0.06],
+    "ecaliso2": [0.24, 0.2, 0.16, 0.14],
+    "1trackiso": [0.14, 0.14, 0.16, 0.16],
+    "2trackiso": [0.2, 0.2, 0.2, 0.2],
+    ####"ecal": [0.1, 0.08, 0.06, 0.06],
+    ####"track": [0.03, 0.04, 0.114, 0.032],
+    ####"hoeXe": [0.066, 0.14, 0.3, 0.5]
 }
 ea_info = {}  # New dict to store ea values and formulas per (var, pt_eta_key)
 
 # Initialize histograms for three pt ranges
 # -----------------------------------------
 histos = {}
-pt_eta_ranges = ["20-50_EB", "20-50_EE", "10-20_EB", "10-20_EE", "5-10_EB", "5-10_EE"]
+pt_eta_ranges = ["20-50_EB_0-1", "20-50_EB_1-1p5", "20-50_EE_1p5-2p1", "20-50_EE_2p1-2p5", "10-20_EB_0-1", "10-20_EB_1-1p5", "10-20_EE_1p5-2p1", "10-20_EE_2p1-2p5", "5-10_EB_0-1", "5-10_EB_1-1p5", "5-10_EE_1p5-2p1", "5-10_EE_2p1-2p5"]
+
 for var, cfg in variables.items():
     histos[var] = {}
     for pt_eta in pt_eta_ranges:
@@ -85,12 +86,20 @@ for root_file in root_files:
             else:
                 continue
             
-            region = "EB" if abs(eta) <= 1.479 else "EE"
+            #region = "EB" if abs(eta) <= 1.479 else "EE"
+            if abs(eta) <= 1.0:
+                region = "EB_0-1"
+            elif (abs(eta) > 1.0 and abs(eta) <= 1.479):
+                region = "EB_1-1p5"
+            elif (abs(eta) > 1.479 and abs(eta) <= 2.1):
+                region = "EE_1p5-2p1"
+            elif (abs(eta) > 2.1 and abs(eta) <= 2.5):
+                region = "EE_2p1-2p5"
+
             pt_eta_key = f"{pt_range}_{region}"
             
-            
             # Find the index of the eta bin
-            eta_index = next((i for i in range(len(ea_eta_bins)) if abs(eta) < ea_eta_bins[i]), len(ea_eta_bins) - 1)
+            eta_index = next((i for i in range(len(ea_eta_bins)-1) if ea_eta_bins[i] <= abs(eta) < ea_eta_bins[i+1]), len(ea_eta_bins)-2)
         
             for var, cfg in variables.items():
                 value = getattr(tree, f"probe_ele_ele32wptight_{var}_HLT")
@@ -100,33 +109,35 @@ for root_file in root_files:
                     formula_str = ""
                     if var == "hoe":
                         ea = ea_values["hoe"][eta_index]
+                        #print(f"eta = {eta:.3f}, bin = [{ea_eta_bins[eta_index]}, {ea_eta_bins[eta_index+1]}), ea = {ea}")
                         val_uncorr = (val * pt * math.cosh(eta) - tree.hlt_rho * ea) / (pt * math.cosh(eta))
-                        formula_str = r"(H/E $\cdot$ E - $\rho$ $\cdot$ {ea:.3f}) / E".format(ea=ea)
+                        formula_str = r"(H/E $\cdot p_T$ - $\rho$ $\cdot$ {ea:.3f}) / $p_T$".format(ea=ea)
                     #elif var == "hoeXe":
                     #    ea = ea_values["hoeXe"][eta_index]
                     #    val_uncorr = (val * pt * math.cosh(eta) - tree.hlt_rho * ea) / (pt * math.cosh(eta))
                     #    formula_str = r"(H/E $\cdot$ E - $\rho$ $\cdot$ {ea:.3f}) / E".format(ea=ea)
                     else:
-                        if "ecal" in var:
-                            ea = ea_values["ecal"][eta_index]
-                        elif "hcal" in var:
+                        if "hcal" in var:
                             ea = ea_values["hcal"][eta_index]
-                        elif "track" in var:
-                            ea = ea_values["track"][eta_index]
-                        elif "eCalPf1" in var:
-                            ea = ea_values["eCalPf1"][eta_index]
-                        elif "eCalPf2" in var:
-                            ea = ea_values["eCalPf2"][eta_index]
-                        elif "trIso1" in var:
-                            ea = ea_values["trIso1"][eta_index]
-                        elif "trIso2" in var:
-                            ea = ea_values["trIso2"][eta_index]
+                        elif "ecaliso1" in var:
+                            ea = ea_values["ecaliso1"][eta_index]
+                        elif "ecaliso2" in var:
+                            ea = ea_values["ecaliso2"][eta_index]
+                        elif "1trackiso" in var:
+                            ea = ea_values["1trackiso"][eta_index]
+                        elif "2trackiso" in var:
+                            ea = ea_values["2trackiso"][eta_index]
+                        #elif "ecal" in var:
+                        #    ea = ea_values["ecal"][eta_index]
+                        #elif "trackiso" in var:
+                        #    ea = ea_values["track"][eta_index]
                         else:
                             ea = 0.0                            
 
                         formula_str = r"${var} \cdot p_T - \rho \cdot {ea:.3f}$".format(var=var, ea=ea)
                         val_uncorr = val * pt - tree.hlt_rho * ea
                         
+
                     # Store only the first time for each var-bin key
                     ea_info.setdefault(var, {}).setdefault(pt_eta_key, formula_str)
 
@@ -143,12 +154,23 @@ import numpy as np
 mplhep.style.use("CMS")  
 
 colors = {
-    "5-10_EB": "royalblue",
-    "5-10_EE": "skyblue",
-    "10-20_EB": "mediumseagreen",
-    "10-20_EE": "limegreen",
-    "20-50_EB": "tomato",
-    "20-50_EE": "coral",
+    # 5-10 GeV
+    "5-10_EB_0-1":        "royalblue",
+    "5-10_EB_1-1p5":      "cornflowerblue",
+    "5-10_EE_1p5-2p1":    "skyblue",
+    "5-10_EE_2p1-2p5":    "lightblue",
+
+    # 10-20 GeV
+    "10-20_EB_0-1":       "mediumseagreen",
+    "10-20_EB_1-1p5":     "mediumaquamarine",
+    "10-20_EE_1p5-2p1":   "springgreen",
+    "10-20_EE_2p1-2p5":   "palegreen",
+
+    # 20-50 GeV
+    "20-50_EB_0-1":       "tomato",
+    "20-50_EB_1-1p5":     "lightsalmon",
+    "20-50_EE_1p5-2p1":   "coral",
+    "20-50_EE_2p1-2p5":   "moccasin",
 }
 
 for var, cfg in variables.items():
